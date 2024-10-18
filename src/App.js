1cabaf8471c6634970ba3d658c4e534db7777d26
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2024-10-18 12:41:14 -0700

    create ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app-background">
+            <div className="canvas">
+                <div className="row">
+                    <button className="col cell border-bottom border-end border-5 border-black"></button>
+                    <button className="col cell border-bottom border-end border-5 border-black"></button>
+                    <button className="col cell border-bottom border-5 border-black"></button>
+                </div>
+                <div className="row">
+                    <button className="col cell border-bottom border-end border-5 border-black"></button>
+                    <button className="col cell border-bottom border-end border-5  border-black"></button>
+                    <button className="col cell border-bottom border-5 border-black"></button>
+                </div>
+                <div className="row">
+                    <button className="col cell border-end border-5 border-black"></button>
+                    <button className="col cell border-end border-5 border-black"></button>
+                    <button className="col cell"></button>
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
